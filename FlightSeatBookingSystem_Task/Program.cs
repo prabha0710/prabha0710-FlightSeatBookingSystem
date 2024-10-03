f568a5cf07dfb359b9eb91bff0996edec7f3e614
PrabhaK <kprabha0710@gmail.com>
2024-10-03 17:44:17 +0530

    Modified : Logic Files

--- FlightSeatBookingSystem_Task/Program.cs
+++ FlightSeatBookingSystem_Task/Program.cs
@@ -1,73 +1,139 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Reflection.PortableExecutable;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace FlightSeatBookingSystem_Task
 {
-	internal class Program
+	public static class Program
 	{
+		
+		
+
+		static List<string> list = new List<string>();
+		static string[] array = new string[50];
+		
 		static void Main(string[] args)
 		{
 
-			string status = "Book";
-			BookFlight(status);
+			
+			BookFlight();
+
+		}
+		
+		private static  string[] Layoutgenerate()
+		{
+			 string[] Characters = new string[5] { "A", "B", "C", "D", "E" };
+			string[,] seats;
+			Console.WriteLine("W    " + "M   " + "A   " + "A   " + "W   ");
+			for(int i = 9; i >= 0; i--)
+			{
+				for(int j = 0; j < Characters.Length; j++)
+				{
+					Console.Write(seats[i, j] = $"{(i + 1) + Characters[j]}" + "   ");
+
+				}
+				Console.WriteLine();
+
+			}
 		}
 
-	
+		private static void BookFlight()
+		{
+			bool loopstring = true;
+			while(loopstring)
+			{
+				array = list.ToArray();
+				Console.WriteLine("Enter a 1 for Book , 2 for Exist , Othervise please type a Requiremen1");
 
-		public static void BookFlight(string status)
+				int Status = int.Parse(Console.ReadLine());
+				if(Status == 1)
+				{
+					string[] seats = Layoutgenerate();
+					Console.WriteLine("Please Enter a Seat Number \n W-Window \n M-Middle \n A- Aisle");
+					string seatNo = Console.ReadLine();
+
+					Checkseat(seatNo, seats);
+
+				}
+				else
+				{
+					Console.WriteLine("Thank You !");
+					loopstring = false;
+				}
+
+			}
+		}
+
+		private static void Checkseat(string? seatNo, string[] seats)
+		{
+			if(array.Count() != 0)
+			{
+				int i = 0;
+				while(i < array.Length)
+				{
+					if(array[i] == seatNo)
+					{
+						Console.WriteLine("Seat is already Booked");
+						ShowAvaliableSeat(seats, array);
+						break;
+					}
+
+					else
+					{
+						bookseat(seatNo);
+						break;
+					}
+
+					i++;
+				}
+			}
+			else
+			{
+
+				bookseat(seatNo);
+
+			}
+		}
+
+		private static void ShowAvaliableSeat(string[] seats, string[] array)
 		{
 
-
-			string[,] seats = { { "10A", "10B", "10C","10D","10E" }  ,
-								{ "9A", "9B", "9C","9","9E" },
-								{ "8A", "8B", "8C","8D","8E" },
-								{ "7A", "7B", "7C","7D","7E" },
-								{ "6A", "6B", "6C","6D","6E" },
-							   { "5A", "5B", "5C","5D","5E" },
-								{ "4A", "4B", "4C","4D","4E" } ,
-								{ "3A", "3B", "3C","3D","3E" },
-								 { "2A", "2B", "2C","2D","2E" },
-								  { "1A", "1B", "1C","1D","1E" },};
-
-			
-
-			Console.WriteLine("Welcome To Flights Booking");
-			Console.WriteLine("Please type 1 if you want to book Flight \n Otherwise Please specify the requirement");
-			int Status = int.Parse(Console.ReadLine());
-			int[] booked = new booked[50];
-			if(Status == 1)
+			Console.WriteLine("Please select the seat from the avaliable seat");
+			Console.WriteLine("Booked Seats a Marked as a X");
+			for(int i = 0; i < 10; i++)
 			{
-				Console.WriteLine("W    " + "M   " + "A   " + "A   " + "W   ");
-				for(int i = 0; i < 10; i++)
+				for(int j = 0; j < 5; j++)
 				{
-					for(int j = 0; j < 5; j++)
+					if((array.Length >= j) && (array[j] != seats[i, j]))
 					{
-						Console.Write(seats[i, j] +"   ");
+						seats[i, j] = seats[i, j] + "(X)";
 					}
-					Console.WriteLine();
+					else
+					{
+						seats[i, j] = seats[i, j] + "(A)";
+					}
+
 				}
-				Console.WriteLine("Please Enter a Seat Number \n W-Window \n M-Middle \n A- Aisle") ;
-
-				string seatsNo = Console.ReadLine();
-				bool Avaliable = true; 
-				
-					int n=1;
-					for(int i=0;i<n;i++)
-					{
-						if(booked[i] != 0)
-						{
-						n++;
-						}
-						booked[i] += int.Parse(seatsNo);
-						Console.WriteLine("Booked");
-					}
-				
-
-			
-				
 			}
-
+			Console.WriteLine("W    " + "M   " + "A   " + "A   " + "W   ");
+			for(int i = 0; i < 10; i++)
+			{
+				for(int j = 0; j < 5; j++)
+				{
+					Console.Write(seats[i, j] + "   ");
+				}
+				Console.WriteLine();
+			}
 		}
+
+		public static void bookseat(string seatNo)
+		{
+			list.Add(seatNo);
+			array = list.ToArray();
+			Console.WriteLine("Booked");
+		}
+		
 	}
-
-
 }
